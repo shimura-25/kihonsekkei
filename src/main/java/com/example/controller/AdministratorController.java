115e433823b105fb9dc80e938b098e1883e8f2e3
shimura-25 <mio.shimura@rakus-partners.co.jp>
2025-02-04 16:01:24 +0900

    ex3

--- src/main/java/com/example/controller/AdministratorController.java
+++ src/main/java/com/example/controller/AdministratorController.java
@@ -3,6 +3,7 @@ package com.example.controller;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,6 +16,7 @@ import com.example.form.LoginForm;
 import com.example.service.AdministratorService;
 
 import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
 
 
 /**
@@ -73,7 +75,17 @@ public class AdministratorController {
 	 * @return ログイン画面へリダイレクト
 	 */
 	@PostMapping("/insert")
-	public String insert(InsertAdministratorForm form) {
+	public String insert(@Valid InsertAdministratorForm form, BindingResult bindingResult) {
+		
+
+		if (administratorService.isMailAddressDuplicate(form.getMailAddress())) {
+			bindingResult.rejectValue("mailAddress", "error.duplicate", "メールアドレスが重複しています");
+		}
+
+		if(bindingResult.hasErrors()){
+			return "administrator/insert";
+		}
+		
 		Administrator administrator = new Administrator();
 		// フォームからドメインにプロパティ値をコピー
 		BeanUtils.copyProperties(form, administrator);
